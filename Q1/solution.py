6084f85225327ce4e0bd73357dc134404a09a5bc
kenwu567 <kenwu567@connect.hku.hk>
2022-11-12 11:02:39 +0800

    second change

--- Q1/solution.py
+++ Q1/solution.py
@@ -3,14 +3,30 @@ total_size_in_shop = input()
 total_request_num = input()
 total_request_size = input()
 
+
+def getValue(mylist):
+    value = 0
+    for i in mylist:
+        if i.find("S", -1) != -1:
+            value += 0.9 * len(i)
+        elif i.find("M", -1) != -1:
+            value += 1
+        else:
+            value += 1.1 * len(i)
+    return value
+
+
 if int(total_request_num) > int(total_num_in_shop):
     print("No")
 else:
+    value = 0
     total_size_in_shop = total_size_in_shop.split(" ")
-    total_size_in_shop = set(total_size_in_shop)
     total_request_size = total_request_size.split(" ")
-    total_request_size = set(total_request_size)
-    if total_request_size.issubset(total_size_in_shop):
+    shopValue = getValue(total_size_in_shop)
+    resquestValue = getValue(total_request_size)
+    if shopValue > resquestValue:
         print("Yes")
     else:
         print("No")
+
+
