c808dac7b623d1a891a06df04d4e053d0b6a78b1
kenwu565656 <78916508+kenwu565656@users.noreply.github.com>
2022-11-12 16:16:06 +0800

    Update solution.py

--- Q1/solution.py
+++ Q1/solution.py
@@ -3,16 +3,14 @@ total_size_in_shop = input()
 total_request_num = input()
 total_request_size = input()
 
-
-def getValue(mylist):
+def getValue(i):
     value = 0
-    for i in mylist:
-        if i.find("S", -1) != -1:
-            value += 0.9 ** len(i)
-        elif i.find("M", -1) != -1:
-            value += 1
-        else:
-            value += 1.1 ** len(i)
+    if i.find("S", -1) != -1:
+        value += 0.9 ** len(i)
+    elif i.find("M", -1) != -1:
+        value += 1
+    else:
+        value += 1.1 ** len(i)
     return value
 
 
@@ -21,11 +19,17 @@ if int(total_request_num) > int(total_num_in_shop):
 else:
     total_size_in_shop = total_size_in_shop.split(" ")
     total_request_size = total_request_size.split(" ")
-    shopValue = getValue(total_size_in_shop)
-    resquestValue = getValue(total_request_size)
-    if shopValue >= resquestValue:
-        print("Yes")
-    else:
-        print("No")
+    different = int(total_num_in_shop) - int(total_request_num)
+    for i in total_size_in_shop:
+        i = getValue(i)
+    for x in total_request_size:
+        x = getValue(x)
+    total_size_in_shop.sort()
+    total_request_size.sort()
+    for i in range(int(total_request_num)):
+        if total_size_in_shop[i + different] < total_request_size[i]:
+            print("No")
+            return
+    print("Yes")
 
 
